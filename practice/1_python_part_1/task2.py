72cd8bad3260c752d76f2f1af864d47f4171f53c
arbalan33 <arbalan@griddynamics.com>
2025-03-21 15:25:44 +0200

    solve 2

--- practice/1_python_part_1/task2.py
+++ practice/1_python_part_1/task2.py
@@ -10,7 +10,30 @@ Examples:
     {'a': 5}
 """
 from typing import Dict
+import unittest
 
 
 def set_to_dict(dict_to_update: Dict[str, int], **items_to_set) -> Dict:
-    ...
+    for k, v in items_to_set.items():
+        if dict_to_update.get(k) is not None and dict_to_update[k] >= v:
+            continue
+        dict_to_update[k] = v
+    return dict_to_update
+
+
+class TestSetToDict(unittest.TestCase):
+    def test_update_dict_with_higher_values(self):
+        self.assertEqual(set_to_dict({'a': 1, 'b': 2, 'c': 3}, a=0, b=4), {'a': 1, 'b': 4, 'c': 3})
+
+    def test_empty_dict(self):
+        self.assertEqual(set_to_dict({}, a=0), {'a': 0})
+
+    def test_no_updates_needed(self):
+        self.assertEqual(set_to_dict({'a': 5}), {'a': 5})
+
+    def test_multiple_updates(self):
+        self.assertEqual(set_to_dict({'a': 1, 'b': 2}, a=3, b=1), {'a': 3, 'b': 2})
+
+if __name__ == '__main__':
+    unittest.main()
+
