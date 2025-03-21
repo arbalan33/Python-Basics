a64911991e43e3cc1d9d23466affd96840d934d5
arbalan33 <arbalan@griddynamics.com>
2025-03-21 16:42:15 +0200

    solve 5

--- practice/1_python_part_1/task5.py
+++ practice/1_python_part_1/task5.py
@@ -11,6 +11,27 @@ Examples:
     '1 2 3'
 """
 
+from collections import Counter
+
 
 def remove_duplicated_words(line: str) -> str:
-    ...
+    unique_words: list[str] = list(Counter(line.split()).keys())
+    return ' '.join(unique_words)
+
+
+import unittest
+
+class TestRemoveDuplicatedWords(unittest.TestCase):
+    
+    def test_basic_case(self):
+        self.assertEqual(remove_duplicated_words('cat cat dog 1 dog 2'), 'cat dog 1 2')
+    
+    def test_all_duplicates(self):
+        self.assertEqual(remove_duplicated_words('cat cat cat'), 'cat')
+    
+    def test_no_duplicates(self):
+        self.assertEqual(remove_duplicated_words('1 2 3'), '1 2 3')
+    
+if __name__ == '__main__':
+    unittest.main()
+
