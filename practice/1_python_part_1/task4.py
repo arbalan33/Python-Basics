3a1ce2db8babf74953e1f6767de1f59b7210824a
arbalan33 <arbalan@griddynamics.com>
2025-03-21 16:38:24 +0200

    solve 4

--- practice/1_python_part_1/task4.py
+++ practice/1_python_part_1/task4.py
@@ -7,7 +7,25 @@ Examples:
     [1, 4, 7]  # because [1^2, 2^2 - (1^2 - 1), 3^2 - (2^2 - 2)]
 """
 from typing import List
+import unittest
 
 
 def calculate_power_with_difference(ints: List[int]) -> List[int]:
-    ...
+    prev = 0
+    ls = []
+    for i in ints:
+        ls.append(i**2 - (prev**2 - prev))
+        prev = i
+    return ls
+
+
+class TestCalculatePowerWithDifference(unittest.TestCase):
+    
+    def test(self):
+        self.assertEqual(calculate_power_with_difference([1, 2, 3]), [1, 4, 7])
+        self.assertEqual(calculate_power_with_difference([1, 2, 3, 4]), [1, 4, 7, 10])
+
+if __name__ == '__main__':
+    unittest.main()
+
+
