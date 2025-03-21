c7d4d4d2d3603a9d57a9b4d6401d3a308020fc15
arbalan33 <arbalan@griddynamics.com>
2025-03-21 16:27:43 +0200

    solve 3

--- practice/1_python_part_1/task3.py
+++ practice/1_python_part_1/task3.py
@@ -12,8 +12,33 @@ Examples:
     >>> build_from_unique_words(word_number=10)
     ''
 """
-from typing import Iterable
+import unittest
+from collections import Counter
+
+def build_from_unique_words(*lines: tuple[str], word_number: int) -> str:
+    words = []
+    for l in lines:
+        # Counter (dict) remembers insertion order
+        c = Counter(l.split())
+        try:
+            word = list(c.keys())[word_number]
+        except IndexError:
+            continue
+        words.append(word)
+
+    return ' '.join(words)
 
 
-def build_from_unique_words(*lines: Iterable[str], word_number: int) -> str:
-    ...
+class TestBuildFromUniqueWords(unittest.TestCase):
+    def test_valid_cases(self):
+        self.assertEqual(build_from_unique_words('a b c', '1 1 1 2 3', 'cat dog milk', word_number=1), 'b 2 dog')
+        self.assertEqual(build_from_unique_words('a b c', '', 'cat dog milk', word_number=0), 'a cat')
+    
+    def test_no_matching_word_number(self):
+        self.assertEqual(build_from_unique_words('1 2', '1 2 3', word_number=10), '')
+    
+    def test_empty_input(self):
+        self.assertEqual(build_from_unique_words(word_number=10), '')
+
+if __name__ == '__main__':
+    unittest.main()
