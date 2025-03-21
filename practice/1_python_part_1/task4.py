399c2da7506f5b0a8fcfd0094e6723bfd515482d
arbalan33 <arbalan@griddynamics.com>
2025-03-21 16:52:49 +0200

    improve 4

--- practice/1_python_part_1/task4.py
+++ practice/1_python_part_1/task4.py
@@ -19,11 +19,20 @@ def calculate_power_with_difference(ints: List[int]) -> List[int]:
     return ls
 
 
+def functional_version(ints: List[int]) -> List[int]:
+    return [(i**2 - (prev**2 - prev)) for prev, i in zip([0] + ints, ints)]
+
+
 class TestCalculatePowerWithDifference(unittest.TestCase):
     
     def test(self):
         self.assertEqual(calculate_power_with_difference([1, 2, 3]), [1, 4, 7])
         self.assertEqual(calculate_power_with_difference([1, 2, 3, 4]), [1, 4, 7, 10])
+
+
+    def test_functional(self):
+        self.assertEqual(functional_version([1, 2, 3]), [1, 4, 7])
+        self.assertEqual(functional_version([1, 2, 3, 4]), [1, 4, 7, 10])
 
 if __name__ == '__main__':
     unittest.main()
