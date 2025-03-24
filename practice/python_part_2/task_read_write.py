50d45326e7d177fbf96cf6d389ea58d4ac452917
arbalan33 <arbalan@griddynamics.com>
2025-03-24 14:39:16 +0200

    solve 2.inp-out

--- practice/python_part_2/task_read_write.py
+++ practice/python_part_2/task_read_write.py
@@ -13,3 +13,37 @@ Example:
 
     result.txt(content: "23, 78, 3")
 """
+
+from pathlib import Path
+
+
+def read_files_from_dir(directory: str) -> list[str]:
+    contents = []
+    for file_path in Path(directory).iterdir():
+        if file_path.is_file():
+            try:
+                with file_path.open("r") as f:
+                    contents.append(f.read().strip())
+            except (ValueError, FileNotFoundError):
+                continue
+    return contents
+
+
+def safe_int(s: str) -> bool:
+    try:
+        return True
+    except ValueError:
+        return False
+    
+
+def extract_numbers_from_files(directory: str, output_file: str = "result.txt"):
+    contents = read_files_from_dir(directory)    
+    numbers = [int(s) for s in contents if safe_int(s)]
+    output = ', '.join(str(n) for n in numbers)
+
+    with open(output_file, "w") as f:
+        f.write(output)
+
+
+if __name__ == "__main__":
+    extract_numbers_from_files("practice/python_part_2/files")
--- practice/python_testing_3/test_read_write.py
+++ practice/python_testing_3/test_read_write.py
@@ -4,3 +4,38 @@ To write files during tests use temporary files:
 https://docs.python.org/3/library/tempfile.html
 https://docs.pytest.org/en/6.2.x/tmpdir.html
 """
+import uuid
+import string
+from ..python_part_2.task_read_write import extract_numbers_from_files
+
+
+def mock_and_run(nums: list[str], tmp_path) -> str:
+    '''Creates mock files from the list of numbers, writing each element in a temp file,
+    then runs the function and returns the contents of the results file.
+    '''
+    filenames = [str(uuid.uuid4()) + ".txt" for _ in nums]
+
+    for filename, content in zip(filenames, nums):
+        file_path = tmp_path / filename
+        with open(file_path, 'w') as f:
+            f.write(content)
+
+    result_file = tmp_path / "result.txt"
+    extract_numbers_from_files(tmp_path, result_file)
+
+    with open(result_file, 'r') as f:
+        result_content = f.read()
+
+    return result_content
+
+
+def test_extract_numbers_from_files(tmp_path):
+    contents = ["23", "78", "3"]
+    result_content = mock_and_run(contents, tmp_path)
+
+    # The files might have been read in a different order,
+    # So we'll check that the numbers are the same instead
+    result_numbers = result_content.split(', ')
+    
+    assert len(result_numbers) == len(contents)
+    assert all(n in contents for n in result_numbers)
