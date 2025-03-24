56e333952371a706af67fd26ee622ea97a01f0c6
arbalan33 <arbalan@griddynamics.com>
2025-03-24 10:01:20 +0200

    configure for testing

--- practice/python_testing_3/test_task_classes.py
+++ practice/python_testing_3/test_task_classes.py
@@ -3,3 +3,10 @@ Write tests for classes in 2_python_part_2/task_classes.py (Homework, Teacher, S
 Check if all methods working correctly.
 Also check corner-cases, for example if homework number of days is negative.
 """
+
+import pytest
+
+from ..python_part_2 import *
+
+def test_ex():
+    assert 2 == 3
