f50e70ae6fa6121de92624957f5598c04af97ab1
arbalan33 <arbalan@griddynamics.com>
2025-03-25 13:25:57 +0200

    solve 4.3

--- practice/python_part_3/task_3_test.py
+++ practice/python_part_3/task_3_test.py
@@ -10,12 +10,26 @@ Note that address may have several domain levels
     False
 """
 import re
+import pytest
 
 
-def is_http_domain(domain: str) -> bool:
-    ...
+URL_REGEX = re.compile(r"^https?:\/\/[^\s\/$.?#].[^\s]*$")
+
+def is_http_url(url: str) -> bool:
+    return bool(URL_REGEX.match(url))
 
 
-"""
-write tests for is_http_domain function
-"""
+@pytest.mark.parametrize("url, is_valid", [
+    ("http://example.com", True),
+    ("https://example.com", True),
+    ("http://example.com/path?query=value", True),
+    ("https://sub.domain.com:8080/path#anchor", True),
+    ("ftp://example.com", False),  # Wrong protocol
+    ("example.com", False),  # Missing http(s)
+    ("http:/example.com", False),  # a single slash
+    ("http:/example.com/ test", False),  # whitespace after domain
+    ("http://", False),  # Incomplete URL
+    ("http://exa mple.com", False),  # Space in URL
+])
+def test_is_http_url(url, is_valid):
+    assert is_http_url(url) == is_valid
