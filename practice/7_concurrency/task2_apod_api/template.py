8d32d3de01426c0e906dca4045fd9f9cde8453cf
arbalan33 <arbalan@griddynamics.com>
2025-04-01 15:31:58 +0300

    finish 7 (concurrency)

--- practice/7_concurrency/task2_apod_api/template.py
+++ practice/7_concurrency/task2_apod_api/template.py
@@ -1,24 +1,46 @@
-API_KEY = "YOUR_KEY"
+import asyncio
+import requests
+import aiohttp
+from pathlib import Path
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
 APOD_ENDPOINT = 'https://api.nasa.gov/planetary/apod'
-OUTPUT_IMAGES = './output'
+OUTPUT_IMAGES = './practice/7_concurrency/task2_apod_api/output'
 
 
-def get_apod_metadata(start_date: str, end_date: str, api_key: str) -> list:
-    pass
+
+async def fetch_url(session, url):
+    async with session.get(url) as response:
+        filename = Path(OUTPUT_IMAGES) / url.split('/')[-1]
+        with open(filename, 'wb') as f:
+            f.write(await response.read())
+        print(f"Downloaded and saved: {filename}")
 
 
-def download_apod_images(metadata: list):
-    pass
+
+async def download_urls(urls):
+    async with aiohttp.ClientSession() as session:
+        return await asyncio.gather(*(fetch_url(session, url) for url in urls))
 
 
-def main():
-    metadata = get_apod_metadata(
-        start_date='2021-08-01',
-        end_date='2021-09-30',
-        api_key=API_KEY,
-    )
-    download_apod_images(metadata=metadata)
+
+def download_apod_images(query_params: list):
+    res = requests.get(APOD_ENDPOINT, query_params)
+    assert res.status_code == 200
+    content = res.json()
+
+    urls = [i["hdurl"] for i in content if i['media_type'] == 'image']
+    asyncio.run(download_urls(urls))
 
 
 if __name__ == '__main__':
-    main()
+
+    download_apod_images({
+        'start_date': '2021-08-01',    
+        'end_date': '2021-08-10',
+        'api_key': API_KEY,
+    })
