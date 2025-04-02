aec29d8b007013376eee9a96c1100a1c05cdae13
arbalan33 <arbalan@griddynamics.com>
2025-04-02 10:53:57 +0300

    capstone: wrote interpreter

---
+++ practice/capstone/interpreter.py
@@ -0,0 +1,147 @@
+import time
+import uuid
+import json
+import re
+import random
+
+
+'''
+Syntax:
+  Schema looks like a JSON object where values are strings called "field specifiers":
+  schema := '{' (<name> ':' <field_spec>)*  '}'
+  E.g. {"name": "str:"}
+
+  field_spec := \" <field_type> ":" <field_modifier> \"
+
+'''
+
+
+class ParsingError(Exception):
+    """Raised when the input string does not match the expected grammar."""
+    pass
+
+
+def parse_field_modifier(s: str):
+    if s == '':
+        return None
+
+    if s[0] == '[':
+        # convert single quotes to double quotes to parse as json
+        s = s.replace("'", '"')
+
+        try:
+            ls = json.loads(s)
+            assert isinstance(ls, list)
+        except:
+            raise ParsingError('Malformed list in field spec')
+
+        if len(ls) == 0:
+            raise ParsingError('List must have at least one element')
+
+        return ls
+
+    if s == 'rand':
+        return ('rand',)
+
+    match = re.search(r"rand\((\w+),\s*(\w+)\)", s)
+    if match:
+        try:
+            int1, int2 = (int(n) for n in match.groups())
+        except:
+            raise ParsingError('Arguments to rand must be two integers')
+        return ('rand', int1, int2)
+
+    # as a last resort, it must be either a string or an int value
+
+    if s[0].isdigit():
+        try:
+            n = int(s)
+            return n
+        except ValueError:
+            raise ParsingError('Malformed integer value')
+
+    if not s.isalnum():
+        raise ParsingError('String value must be alphanumeric')
+
+    return s
+
+
+def parse_field_spec(s):
+    field_type, field_modifier_s = s.split(':')
+
+    if field_type not in ('int', 'str', 'timestamp'):
+        raise ParsingError('Invalid field type')
+
+    field_modifier = parse_field_modifier(field_modifier_s)
+
+    return (field_type, field_modifier)
+
+
+def parse(s: str):
+    try:
+        obj = json.loads(s)
+        assert isinstance(obj, dict)
+    except:
+        raise ParsingError('Wrong syntax of the key-value mapping')
+
+    if len(obj) == 0:
+        raise ParsingError("At least one field spec is required")
+
+    AST = {name: parse_field_spec(v) for name, v in obj.items()}
+    return AST
+
+
+def evaluate_field_spec(spec: tuple[str, any]):
+    typ, modi = spec
+
+    # timestamp is returned no matter the modifier
+    if typ == 'timestamp':
+        return time.time()
+
+    if modi is None:
+        return '' if typ == 'str' else None
+    
+
+    # literal values are returned as-is (int and str)
+    if isinstance(modi, int):
+        if typ != 'int':
+            raise ParsingError("Wrong field type for int value")
+
+        return modi
+    if isinstance(modi, str):
+        if typ != 'str':
+            raise ParsingError("Wrong field type for str value")
+        return modi
+
+    # random
+    if isinstance(modi, tuple) and modi[0] == 'rand':
+        if typ == 'str':
+            if len(modi) != 1:
+                raise ParsingError('String fields don\'t support rand with range')
+            return str(uuid.uuid4())
+
+        # random ints
+        if typ == 'int' and modi == ('rand',):
+            return random.randint(0, 10000)
+        # wish i could use the match block here
+        if typ == 'int' and modi[0] == 'rand' and len(modi) == 3:
+            _, start, end = modi
+            return random.randint(start, end)
+    
+    # lists
+    if isinstance(modi, list):
+        if typ == 'str':
+            if not all(isinstance(item, str) for item in modi):
+                raise ParsingError('List items of a string field must be strings')
+        
+        if typ == 'int':
+            if not all(isinstance(item, int) for item in modi):
+                raise ParsingError('List items of a string field must be ints')
+            
+        return random.choice(modi)
+            
+    return None
+
+
+def evaluate(ast: dict):
+    return {name: evaluate_field_spec(spec) for name, spec in ast.items()}
---
+++ practice/capstone/test_interpreter.py
@@ -0,0 +1,187 @@
+from interpreter import parse, ParsingError, evaluate
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    ('inp', 'expected_AST'),
+    [
+
+        # 'rand' is a keyword, not a string
+        (
+            '{"name": "timestamp:rand"}',
+            {'name': ('timestamp', ('rand',))}
+        ),
+
+        # 'rand(1, 2)'
+        (
+            '{"name": "timestamp:rand(1, 2)"}',
+            {'name': ('timestamp', ('rand', 1, 2))}
+        ),
+
+        # empty value spec is always None (for any type)
+        (
+            '{"name": "str:"}',
+            {'name': ('str', None)}
+        ),
+
+
+        # list with one item
+        (
+            '''{"name": "str:['item']"}''',
+            {'name': ('str', ['item'])}
+        ),
+
+        # list with two items
+        (
+            '''{"name": "str:['item', 'second']"}''',
+            {'name': ('str', ['item', 'second'])}
+        ),
+
+        # list of ints
+        (
+            '''{"name": "str:[1, 2]"}''',
+            {'name': ('str', [1, 2])}
+        ),
+
+        # string value other than rand
+        (
+            '{"name": "str:name1"}',
+            {'name': ('str', 'name1')}
+        ),
+
+
+        # value spec starts with 'rand' but is just a string
+        (
+            '{"name": "str:random"}',
+            {'name': ('str', 'random')}
+        ),
+
+        # int value
+        (
+            '{"name": "str:1"}',
+            {'name': ('str', 1)}
+        ),
+
+
+        (
+            '''
+         {"date":"timestamp:",
+        "name": "str:rand",
+        "type":"str:['client', 'partner', 'government']",
+        "age": "int:rand(1, 90)",
+        "str": "str:cat1",
+        "num": "int:1"}
+         ''',
+            {'date': ('timestamp', None),
+             'name': ('str', ('rand',)),
+             'type': ('str', ['client', 'partner', 'government']),
+             'age': ('int', ('rand', 1, 90)),
+             'str': ('str', 'cat1'),
+             'num': ('int', 1),
+             }
+        )
+    ])
+def test_parse(inp, expected_AST):
+    assert parse(inp) == expected_AST
+
+
+@pytest.mark.parametrize(
+    "inp",
+    [
+        # at least one field spec required
+        '{}',
+
+        # rand arguments must be int
+        '{"name": "int:rand(a, b)"}',
+
+    # at least one element required in list
+        '''{"name": "str:[]"}''',
+
+        # string value can only contain alphanumeric chars
+        '{"name": "str:a1+"}',
+        
+        # integer value
+        '{"name": "str:1e"}',
+    ])
+def test_raises(inp):
+    with pytest.raises(ParsingError):
+        parse(inp)
+
+
+
+def test_fields_count():
+    ast = {'num': ('int', ('rand',)),
+           'f': ('str', 'test')}
+    res = evaluate(ast)
+    assert isinstance(res, dict)
+    assert list(res.keys()) == ['num', 'f']
+
+
+
+def test_rand_int():
+    ast = {'num': ('int', ('rand',))}
+    res = evaluate(ast)
+    assert isinstance(res['num'], int)
+
+
+    ast = {'num': ('int', ('rand', 1, 1))}
+    res = evaluate(ast)
+    assert res['num'] == 1
+
+
+def test_rand_str():
+    ast = {'f': ('str', ('rand',))}
+    res = evaluate(ast)
+    assert isinstance(res['f'], str)
+
+
+def test_literal_values():
+    ast = {'f': ('str', 'a1')}
+    res = evaluate(ast)
+    assert res['f'] == 'a1'
+
+
+def test_list():
+    ast = {'f': ('str', ['1', '2'])}
+    res = evaluate(ast)
+    assert res['f'] in ['1', '2']
+
+    ast = {'f': ('int', [1, 2])}
+    res = evaluate(ast)
+    assert res['f'] in [1, 2]
+
+
+def test_empty_string():
+    ast = {'f': ('str', None)}
+    res = evaluate(ast)
+    assert res['f'] == ''
+
+
+def test_empty_int():
+    ast = {'f': ('int', None)}
+    res = evaluate(ast)
+    assert res['f'] is None
+
+
+def test_timestamp():
+    ast = {'f': ('timestamp', 'test')}
+    res = evaluate(ast)
+    assert isinstance(res['f'], float)
+
+
+
+
+@pytest.mark.parametrize(
+    "ast",
+    [
+        # all items of a list must have the appropriate type
+        {'f': ('int', ['test', 1])},
+        {'f': ('str', ['test', 1])},
+
+        # can't do rand range for string type
+        {'f': ('str', ('rand', 1, 2))},
+    ])
+def test_evaluate_raises(ast):
+    with pytest.raises(ParsingError):
+        evaluate(ast)
